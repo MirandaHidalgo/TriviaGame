77d63aebb50ba5c0d6b94b6bcf510985163162d7
Surterre <mirandahidalgo21@gmail.com>
2019-10-19 22:45:38 -0400

    final commit. game working, basic

--- trivia.js
+++ trivia.js
@@ -1,11 +1,11 @@
-var mainContainer = $('quiz')
+var panel = $('#gamecontainer')
 
 $(document).on("click", "#start", function (event ) {
-    Game.start()
+    game.start()
 })
 
-$(document).on("click", "#Over", function (event ) {
-    Game.done()
+$(document).on("click", "#done", function (event ) {
+    game.done()
 })
 
 var questions = [{
@@ -22,18 +22,90 @@ var questions = [{
     rightAnswer: "That was easy! This is it!"
 }];
 
-var quiz = {
+
+var game = {
     correct: 0,
     incorrect: 0,
-    counter: 30,
+    counter: 60,
 
-    countdown: function (){
+    countdown: function () {
         game.counter--;
-        $('#counter-number').html(game.counter);
+        $("#counter-number").html(game.counter);
 
-        if (game.counter === 00) {
-            alert("TIME IS OVER!");
-            game.done()
+        if (game.counter === 0) {
+            alert("TIME'S UP");
+            game.done();
+
         }
+    },
+
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+        $('#subcontainer').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2>');
+        $("#start").remove();
+
+        for (var i = 0; i < questions.length; i++) {
+            panel.append('<h2>' + questions[i].question + '</h2>');
+            for (var j = 0; j < questions[i].choices.length; j++) {
+                panel.append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
+            }
+        }
+        panel.append("<button id='done'>DONE</button>");
+
+    },
+
+    done: function () {
+
+        $.each($("input[name='question-0']:checked"), function () {
+            if ($(this).val() == questions[0].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-1']:checked"), function () {
+            if ($(this).val() == questions[1].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-2']:checked"), function () {
+            if ($(this).val() == questions[2].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-3']:checked"), function () {
+            if ($(this).val() == questions[3].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+
+        this.results();
+    },
+
+
+    results: function () {
+        clearInterval(timer);
+
+        $("#subcontainer h2").remove();
+        panel.html("<h2>You're Done!</h2>");
+        panel.append("<h3>You got this many right: " + this.correct + "</h3>");
+        panel.append("<h3>You got this many wrong: " + this.incorrect + "</h3>");
+        panel.append("<h3>You didn't answer this many: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+
     }
-},
+
+};
